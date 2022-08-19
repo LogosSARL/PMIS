7209b461eeb2bce0dfb738bf50e8bb673454d15e
logosgithub@logos.com.lb <>
2022-08-19 09:29:39 +0300

    Comment @Tareif

--- PLSQL/ACCOUNT_CUSTOMER.sql
+++ PLSQL/ACCOUNT_CUSTOMER.sql
@@ -1,7 +1,7 @@
 --------------------------------------------------------
 --  DDL for Type ACCOUNT_CUSTOMER
 --------------------------------------------------------
-
+--@Tareif
   CREATE OR REPLACE TYPE "PRJ"."ACCOUNT_CUSTOMER" under customer (
      acct_no varchar2(20)
      , credit_limit number
